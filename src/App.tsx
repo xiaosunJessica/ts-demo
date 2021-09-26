bbbc4da182e727e2062f66398c66b55eb4975f08
sunyaqin <>
2021-09-26 19:40:26 +0800

    feat:replace

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
-import * as React from 'react';
-import './App.css';
-import { DemoUseSelector } from './pages/DemoUseSelector';
-
+import * as React from "react";
+import "./App.css";
+import { DemoUseSelector } from "./pages/DemoUseSelector";
+import SliderLogin from "./pages/slider-login";
 class App extends React.Component {
   public render() {
     return (
       <div className="App">
-				<DemoUseSelector />
+        <DemoUseSelector />
+        <SliderLogin />
       </div>
     );
   }
--- src/reducers/index.tsx
+++ src/reducers/index.tsx
@@ -1,19 +1,22 @@
-import { createStore } from 'redux';
-import { selector } from './selector';
-import { StoreState } from '../types/index';
+import { createStore } from "redux";
+import { selector } from "./selector";
+import { StoreState } from "../types/index";
 
-const store = createStore<StoreState, any, any, any>(selector, {
-	count: 1,
-	countObj: {
-		count: 1
-	},
-	todos: [{
-		text: 'todo0',
-		id: 0
-	}, {
-		text: 'todo1',
-		id: 1
-	}]
+const store = createStore<any, any, any, any>(selector, {
+  count: 1,
+  countObj: {
+    count: 1,
+  },
+  todos: [
+    {
+      text: "todo0",
+      id: 0,
+    },
+    {
+      text: "todo1",
+      id: 1,
+    },
+  ],
 });
 
 export default store;
